56decc6f552465cc3d01f1582268cfd6541623ad
jn1za <zarajc@hawaii.edu>
2025-11-08 22:06:46 -1000

    Cleanup

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -15,6 +15,8 @@ const AdminPage = async () => {
 
   const contacts = await prisma.contact.findMany({});
 
+  const notes = await prisma.note.findMany({});
+
   return (
     <main>
       <Container id="list" fluid className="py-3">
@@ -24,7 +26,7 @@ const AdminPage = async () => {
             <Row xs={1} md={2} lg={3} className="g-4">
               {contacts.map((contact) => (
                 <Col key={contact.firstName + contact.lastName}>
-                  <ContactCardAdmin contact={contact} />
+                  <ContactCardAdmin contact={contact} notes={notes.filter(note => (note.contactId === contact.id))} />
                 </Col>
               ))}
             </Row>
--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,15 +1,13 @@
 /* eslint-disable operator-linebreak */
 import { getServerSession } from 'next-auth';
 import { Col, Container, Row } from 'react-bootstrap';
-// import { prisma } from '@/lib/prisma';
-// import StuffItem from '@/components/StuffItem';
 import { loggedInProtectedPage } from '@/lib/page-protection';
 import authOptions from '@/lib/authOptions';
 import { Contact } from '@prisma/client';
 import ContactCard from '@/components/ContactCard';
 import { prisma } from '@/lib/prisma';
 
-/** Render a list of stuff for the logged in user. */
+/** Render a list of contact cards for the logged in user. */
 const ListPage = async () => {
   // Protect the page, only logged in users can access it.
   const session = await getServerSession(authOptions);
@@ -19,14 +17,6 @@ const ListPage = async () => {
       // eslint-disable-next-line @typescript-eslint/comma-dangle
     } | null,
   );
-  // const owner = (session && session.user && session.user.email) || '';
-  // const stuff = await prisma.stuff.findMany({
-  //   where: {
-  //     owner,
-  //   },
-  // });
-  // console.log(stuff);
-
   const owner = session?.user?.email || '';
   const contacts: Contact[] = await prisma.contact.findMany({
     where: {
